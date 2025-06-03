8886b03d965283e6f23bac4a35018281632030b4
Almeid Hodzic <almeid.hodzic@litmus.io>
2025-06-03 17:23:59 +0200

    LIT-2881 Changes in versions.tf

--- modules/aws-module/versions.tf
+++ modules/aws-module/versions.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5"
     }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "2.3.6"
+    }
   }
   required_version = ">= 1.5.7"
 }
