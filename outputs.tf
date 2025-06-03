dd04a29137d6dd98bc9c261098ad5cf6e5f3f358
Almeid Hodzic <almeid.hodzic@litmus.io>
2025-06-03 17:23:18 +0200

    LIT-2881 Changes in outputs

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
-output "EC2_PRIVATE_IP" {
-  value = module.edgemanager-aws.private_ip
+output "APP_VERSION" {
+  value = var.app_version
 }
 
-output "EC2_HTTPS_URL" {
+output "ADMIN_APP_HTTPS_URL" {
+  value = "https://${module.edgemanager-aws.private_ip}:8446"
+}
+
+output "APP_HTTPS_URL" {
   value = "https://${module.edgemanager-aws.private_ip}"
 }
 
-output "EC2_SSH" {
-  value = module.edgemanager-aws.ssh_command
-}
-
-output "APP_VERSION" {
-  value = var.app_version
+output "PRIVATE_IP" {
+  value = module.edgemanager-aws.private_ip
 }
