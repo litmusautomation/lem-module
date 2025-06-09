e15f537443888b496c615146edb31fe064de09ec
Almeid Hodzic <almeid.hodzic@litmus.io>
2025-06-09 12:41:51 +0200

    LIT-2881 Add rules and ports in sg for AWS

--- aws/modules/aws-module/sg.tf
+++ aws/modules/aws-module/sg.tf
@@ -1,3 +1,50 @@
+locals {
+  tcp_ports = [
+    80, 443, 8883,
+    9092, 8446, 9093,
+    8123, 8543, 9000,
+    9004, 9090,
+  ]
+
+  udp_ports = [
+    51820, 123
+  ]
+
+  ssh_ports = [
+    22
+  ]
+
+  tcp_rules = [
+    for port in local.tcp_ports : {
+      from_port   = port
+      to_port     = port
+      protocol    = "tcp"
+      description = "tcp-${port}"
+      cidr_blocks = join(",", var.ingress_cidr_blocks)
+    }
+  ]
+
+  udp_rules = [
+    for port in local.udp_ports : {
+      from_port   = port
+      to_port     = port
+      protocol    = "udp"
+      description = "udp-${port}"
+      cidr_blocks = join(",", var.ingress_cidr_blocks)
+    }
+  ]
+
+  ssh_rule = [
+    for port in local.ssh_ports : {
+      from_port   = port
+      to_port     = port
+      protocol    = "tcp"
+      description = "SSH from specific subnets"
+      cidr_blocks = join(",", var.ingress_cidr_ssh_blocks)
+    }
+  ]
+}
+
 module "edgemanager_security_group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 5.0"
@@ -8,106 +55,7 @@ module "edgemanager_security_group" {
   egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      description = "http-80-tcp",
-      cidr_blocks = join(",", var.ingress_cidr_blocks)
-    },
-    {
-      from_port   = 443
-      to_port     = 443
-      protocol    = "tcp"
-      description = "https-443-tcp",
-      cidr_blocks = join(",", var.ingress_cidr_blocks)
-    },
-    {
-      from_port   = 8883
-      to_port     = 8883
-      protocol    = "tcp"
-      description = "activemq-8883-tcp",
-      cidr_blocks = join(",", var.ingress_cidr_blocks)
-    },
-    {
-      from_port   = 9092
-      to_port     = 9092
-      protocol    = "tcp"
-      description = "kafka-broker-tcp",
-      cidr_blocks = join(",", var.ingress_cidr_blocks)
-    },
-    {
-      from_port   = 8446
-      to_port     = 8446
-      protocol    = "tcp"
-      description = "INSIGHTS, MINIO AND LOOP_ADMIN",
-      cidr_blocks = join(",", var.ingress_cidr_blocks)
-    },
-    {
-      from_port   = 9093
-      to_port     = 9093
-      protocol    = "tcp"
-      description = "KAFKA_SSL"
-      cidr_blocks = join(",", var.ingress_cidr_blocks)
-    },
-    {
-      from_port   = 8123
-      to_port     = 8123
-      protocol    = "tcp"
-      description = "CLICKHOUSE_HTTP"
-      cidr_blocks = join(",", var.ingress_cidr_blocks)
-    },
-    {
-      from_port   = 8543
-      to_port     = 8543
-      protocol    = "tcp"
-      description = "CLICKHOUSE_HTTP_SSL"
-      cidr_blocks = join(",", var.ingress_cidr_blocks)
-    },
-    {
-      from_port   = 9000
-      to_port     = 9000
-      protocol    = "tcp"
-      description = "CLICKHOUSE_NATIVE"
-      cidr_blocks = join(",", var.ingress_cidr_blocks)
-    },
-    {
-      from_port   = 9004
-      to_port     = 9004
-      protocol    = "tcp"
-      description = "CLICKHOUSE_MYSQL"
-      cidr_blocks = join(",", var.ingress_cidr_blocks)
-    },
-    {
-      from_port   = 51820
-      to_port     = 51820
-      protocol    = "udp"
-      description = "WIREGUARD"
-      cidr_blocks = join(",", var.ingress_cidr_blocks)
-    },
-    {
-      from_port   = 123
-      to_port     = 123
-      protocol    = "udp"
-      description = "NTP"
-      cidr_blocks = join(",", var.ingress_cidr_blocks)
-    },
-    {
-      from_port   = 9090
-      to_port     = 9090
-      protocol    = "tcp"
-      description = "PROMETHEUS"
-      cidr_blocks = join(",", var.ingress_cidr_blocks)
-    },
-    # {
-    #   from_port   = 22
-    #   to_port     = 22
-    #   protocol    = "tcp"
-    #   description = "SSH from specified subnets"
-    #   cidr_blocks = join(",", var.ingress_cidr_ssh_blocks)
-    # },
-  ]
+  ingress_with_cidr_blocks = concat(local.tcp_rules, local.udp_rules)
 
   tags = merge(local.tags, var.tags)
 }
