63309c7acc5b2083da7a12401b86d2dc154f917b
almeidhodzic <140192422+almeidhodzic@users.noreply.github.com>
2026-03-18 17:31:37 +0100

    LIT-5811 Switch to public repository (#5)
    * LIT-5811 Switch to public repository
    
    * LIT-5811 Use more recent EM version

--- aws/examples/edgemanager-aws/main.tf
+++ aws/examples/edgemanager-aws/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "edgemanager-example" {
-  source      = "git@github.com:litmusautomation/lem-module//aws?ref=main"
+  source      = "github.com/litmusautomation/lem-module//aws?ref=main"
   name        = var.name
   oem_name    = var.oem_name    # name of the edge manager
   app_version = var.app_version # version of the application
