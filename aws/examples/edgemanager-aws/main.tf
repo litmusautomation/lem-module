98ccc790bc6dd9c0f3c19b58c6e70f92ed7e1da8
almeidhodzic <140192422+almeidhodzic@users.noreply.github.com>
2026-03-19 14:41:30 +0100

    Reference main branch (#8)
    * Reference main branch

--- aws/examples/edgemanager-aws/main.tf
+++ aws/examples/edgemanager-aws/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "edgemanager-example" {
-  source      = "github.com/litmusautomation/lem-module//aws?ref=LIT-5811-code-improvements"
+  source      = "github.com/litmusautomation/lem-module//aws?ref=main"
   name        = var.name
   oem_name    = var.oem_name
   app_version = var.app_version
